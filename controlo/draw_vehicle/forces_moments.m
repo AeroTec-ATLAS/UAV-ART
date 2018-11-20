8d725e1a0383b2e8bb09aea21c9ab6f490bc9c3d
Leonardo Pedroso <40807922+LeonardoPedroso@users.noreply.github.com>
2018-11-20 09:47:50 +0000

    Update forces_moments.m

--- controlo/draw_vehicle/forces_moments.m
+++ controlo/draw_vehicle/forces_moments.m
@@ -10,14 +10,9 @@
 %       wind  - wind vector in the inertial frame
 %
 
-function [F,M] = forces_moments(x,delta, wind,R_vb,P)
-    
-    %% Init output
-    
-    F = zeros(3,1);
-    M = zeros(3,1);
+function out = forces_moments(x, delta, wind, P)
 
-    %% Relabel the inputs
+    % relabel the inputs
     pn      = x(1);
     pe      = x(2);
     pd      = x(3);
@@ -42,11 +37,12 @@ function [F,M] = forces_moments(x,delta, wind,R_vb,P)
     w_wg    = wind(6); % gust along body z-axis
     
     % compute wind data in NED
-    w_n = 0;
-    w_e = 0;
-    w_d = 0;
+    w_n = w_ns;
+    w_e = w_es;
+    w_d = w_ds;
     
     %% Compute air data
+    
     Va = sqrt(u^2+v^2+w^2);
     if(abs(u) > eps)
         alpha = atan2(w,u);
@@ -58,6 +54,7 @@ function [F,M] = forces_moments(x,delta, wind,R_vb,P)
     else
         beta = 0;
     end
+
     %% Compute external forces and torques on aircraft
     
     if(Va > eps)
@@ -69,25 +66,26 @@ function [F,M] = forces_moments(x,delta, wind,R_vb,P)
         F_drag = 0;
         F(2) = 0;
     end
-    
+
     F_prop = 0.5*P.rho*P.S_prop*P.C_prop*((P.k_motor*delta_t)^2-Va^2);
-    
     T_ld_xy = [cos(alpha)   -sin(alpha);...
                sin(alpha)   cos(alpha)]; 
-    
     Faer = transpose(T_ld_xy*[-F_lift;-F_drag]+[F_prop; 0]);
     
-    F(1) = Faer(1);
-    F(3) = Faer(2);
+    Force = zeros(3,1);
+    Torque = zeros(3,1);
 
-    F = F + R_vb*[0; 0; P.m*P.g];
-    
+    Force(1) = Faer(1);
+    Force(3) = Faer(2);
+    Force = Force + R_vb*[0; 0; P.m*P.g];
+
     if(Va > eps)
-        M(1) = 0.5*P.rho*Va^2*P.S_wing*P.b*(P.C_ell_0 + P.C_ell_beta*beta + P.C_ell_p*(P.b/(2*Va))*p + P.C_ell_r*(P.b/(2*Va))*r + P.C_ell_delta_a*delta_a + P.C_ell_delta_r*delta_r) - P.k_T_P*(P.k_Omega*delta_t)^2;
-        M(2) = 0.5*P.rho*Va^2*P.S_wing*P.c*(P.C_m_0 + P.C_m_alpha*alpha + P.C_m_q*(P.c/(2*Va))*q + P.C_m_delta_e*delta_e);  
-        M(3) = 0.5*P.rho*Va^2*P.S_wing*P.b*(P.C_n_0 + P.C_n_beta*beta + P.C_n_p*(P.b/(2*Va))*p + P.C_n_r*(P.b/(2*Va))*r + P.C_n_delta_a*delta_a + P.C_n_delta_r*delta_r);
+        Torque(1) = 0.5*P.rho*Va^2*P.S_wing*P.b*(P.C_ell_0 + P.C_ell_beta*beta + P.C_ell_p*(P.b/(2*Va))*p + P.C_ell_r*(P.b/(2*Va))*r + P.C_ell_delta_a*delta_a + P.C_ell_delta_r*delta_r) - P.k_T_P*(P.k_Omega*delta_t)^2;
+        Torque(2) = 0.5*P.rho*Va^2*P.S_wing*P.c*(P.C_m_0 + P.C_m_alpha*alpha + P.C_m_q*(P.c/(2*Va))*q + P.C_m_delta_e*delta_e);  
+        Torque(3) = 0.5*P.rho*Va^2*P.S_wing*P.b*(P.C_n_0 + P.C_n_beta*beta + P.C_n_p*(P.b/(2*Va))*p + P.C_n_r*(P.b/(2*Va))*r + P.C_n_delta_a*delta_a + P.C_n_delta_r*delta_r);
     end
+   
+    out = [Force'; Torque'; Va; alpha; beta; w_n; w_e; w_d];
 end
-
 
 
