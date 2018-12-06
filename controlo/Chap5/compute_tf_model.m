666c0eda558ba804a861e3c593495863bbb4449b
jschpinto <44210995+jschpinto@users.noreply.github.com>
2018-12-06 17:24:56 +0000

    Add files via upload

--- controlo/Chap5/compute_tf_model.m
+++ controlo/Chap5/compute_tf_model.m
@@ -1,39 +1,42 @@
 function [T_phi_delta_a,T_chi_phi,T_theta_delta_e,T_h_theta,T_h_Va,T_Va_delta_t,T_Va_theta,T_v_delta_r]...
     = compute_tf_model(x_trim,u_trim,P)
-% x_trim is the trimmed state,
-% u_trim is the trimmed input
-% add stuff here
 
-% x_trim(4)
-% 
-% a_phi2 = (1/2)*P.rho*x_trim(4)^2*P.S*P.b*(P.g3*P.C_ell_delta_a+P.g4*P.C_n_delta_a);
-% a_phi1 = (-1/4)*P.rho*x_trim(4)*P.S*P.b^2*(P.g3*P.C_ell_p+P.g4*P.C_n_p);
-% a_theta1 = (-1/(4*P.Jy))*P.rho*x_trim(4)*P.c^2*P.S*P.C_m_q;
-% a_theta2 = (-1/(2*P.Jy))*P.rho*x_trim(4)^2*P.c*P.S*P.C_m_alpha;
-
-a_phi2 = 0;
-a_phi1 = 0;
-a_theta1 = 0;
-a_theta2 = 0;
-a_theta3 = 0;
-a_V2 = 0;
-a_V1 = 0;
-a_V3 = 0;
-a_beta2 = 0;
-a_beta1 = 0;
-
-
-Va_trim = x_trim(4); %% CALcular velocidade decentemenye
-theta_trim = x_trim(8);
+    % x_trim is the trimmed state,
+    % u_trim is the trimmed input
+    Va_trim = sqrt(sum(x_trim(4:6).^2));
+    theta_trim = x_trim(8);
+    chi_trim = x_trim(9); % No wind chi = psi (trim condition)
+    alpha_trim = atan(x_trim(6)/x_trim(4));
     
-% define transfer functions
-T_phi_delta_a   = tf([a_phi2],[1,a_phi1,0]);
-T_chi_phi       = tf([P.gravity/Va_trim],[1,0]);
-T_theta_delta_e = tf(a_theta3,[1,a_theta1,a_theta2]);
-T_h_theta       = tf([Va_trim],[1,0]);
-T_h_Va          = tf([theta_trim],[1,0]);
-T_Va_delta_t    = tf([a_V2],[1,a_V1]);
-T_Va_theta      = tf([-a_V3],[1,a_V1]);
-T_v_delta_r     = tf([Va_trim*a_beta2],[1,a_beta1]);
+    delta_e_trim = u_trim(1);
+    delta_t_trim = u_trim(4);
+
+    a_phi1 = -0.5*P.rho*Va_trim^2*P.S_wing*P.b^2*P.C_p_p/(2*Va_trim);
+    a_phi2 = -0.5*P.rho*Va_trim^2*P.S_wing*P.b*P.C_p_delta_a;
+    
+    a_theta1 = -P.rho*Va_trim*P.c^2*P.S_wing*P.C_m_q/4*P.Jy;
+    a_theta2 = -P.rho*Va_trim^2*P.c^2*P.S_wing*P.C_m_alpha/2*P.Jy;
+    a_theta3 = P.rho*Va_trim^2*P.c^2*P.S_wing*P.C_m_delta_e/2*P.Jy;
+    
+    a_beta1 = -P.rho*Va_trim*P.S_wing*C_Y_beta/2*P.mass;
+    a_beta2 = P.rho*Va_trim*P.S_wing*C_Y_delta_r/2*P.mass;
+    
+    a_V1 = P.rho*Va_trim*P.S_wing*(P.C_D_0 + P.C_D_alpha*alpha_trim + ...
+                    P.C_D_delta_e*delta_e_trim) + P.rho*S_prop*Va_trim;
+                
+    a_V2 = P.rho*P.S_prop*P.C_prop*P.k_motor^2*delta_t_trim;
+    
+    a_V3 = P.gravity*cos(theta_trim - chi_trim);
+      
+    % define transfer functions
+    T_phi_delta_a   = tf(a_phi2,[1,a_phi1,0]);
+    T_chi_phi       = tf(P.gravity/Va_trim,[1,0]);
+    T_theta_delta_e = tf(a_theta3,[1,a_theta1,a_theta2]);
+    T_h_theta       = tf(Va_trim,[1,0]);
+    T_h_Va          = tf(theta_trim,[1,0]);
+    T_Va_delta_t    = tf(a_V2,[1,a_V1]);
+    T_Va_theta      = tf(-a_V3,[1,a_V1]);
+    T_v_delta_r     = tf(Va_trim*a_beta2,[1,a_beta1]);
 
 end
+
