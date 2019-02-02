a554aa7061d0286543324c5bddd1a7ca89796d3d
Leonardo Pedroso <leonardodalousa@gmail.com>
2019-02-02 09:30:07 +0000

    Meeting 01/02/2019

--- controlo/Chap6_Gain_Tuning/getGains.m
+++ controlo/Chap6_Gain_Tuning/getGains.m
@@ -1,32 +1,29 @@
 function G = getGains(P,G)
-    
     %% Roll gains
     
-    G.delta_a_max = pi/4;
+    G.delta_a_max = pi/6;
     e_phi_max = pi/12;
     
-    G.k_p_roll = G.delta_a_max/e_phi_max
-    
+    G.k_p_roll = 1.2;
+ 
     bandwidth_roll = sqrt(abs(G.a_phi2)*G.k_p_roll);
-    damping_roll = 0.8;
+    damping_roll = 0.9;
     
-    G.k_d_roll = (2*damping_roll*bandwidth_roll-G.a_phi1)/G.a_phi2
+    G.k_d_roll = (2*damping_roll*bandwidth_roll-G.a_phi1)/G.a_phi2;
     
-    %% Root Locus
-%     rootsKi = 0;
-%     while(G.k_i_roll<=500)
-%         p = [1 (G.a_phi1+G.a_phi2*G.k_d_roll) G.a_phi2*G.k_p_roll G.a_phi2*G.k_i_roll];
-%         rootsKi = [rootsKi; roots(p)];
-%         G.k_i_roll = G.k_i_roll + 0.5;
-%     end
-%     
-%     
-%     scatter(real(rootsKi(2:end,1)), imag(rootsKi(2:end,1)),2);
 
-% Conclusion -> the smalest k_i_roll is the better provided that disturbances are rejected 
     
-    G.k_i_roll = 0.6
+    s = tf('s');
+    tf_roll = G.a_phi2/(s*(s^2 + (G.a_phi1 + G.a_phi2*G.k_d_roll)*s + ...
+              G.a_phi2*G.k_p_roll));
+          
+    rlocus(tf_roll);
     
+    
+    %G.k_i_roll = 0.6;
+    G.k_i_roll = 0.2;
+    
+   
     %% Course angle
     
     bandwidth_course = bandwidth_roll/38;
@@ -44,7 +41,7 @@ function G = getGains(P,G)
     bandwidth_sideslip = (G.a_beta2*(G.delta_r_max/sideslip_max)+G.a_beta1)/(2*damping_sideslip);
     
    
-    G.k_p_sideslip = (G.delta_r_max/sideslip_max)
+    G.k_p_sideslip = (G.delta_r_max/sideslip_max);
     G.k_i_sideslip = bandwidth_sideslip^2/abs(G.a_beta2);
     G.k_i_sideslip = 180;
     
@@ -55,11 +52,11 @@ function G = getGains(P,G)
     
     damping_pitch = 0.85;
   
-    G.k_p_pitch = sign(G.a_theta3)*G.delta_e_max/e_theta_max
+    G.k_p_pitch = sign(G.a_theta3)*G.delta_e_max/e_theta_max;
     
     bandwidth_pitch = sqrt(G.a_theta2+G.k_p_pitch*G.a_theta3);
     
-    G.k_d_pitch = (2*damping_pitch*bandwidth_pitch-G.a_theta1)/G.a_theta3
+    G.k_d_pitch = (2*damping_pitch*bandwidth_pitch-G.a_theta1)/G.a_theta3;
     
     k_theta_DC = G.k_p_pitch*G.a_theta3/(G.a_theta2+G.k_p_pitch*G.a_theta3);
     
@@ -69,8 +66,8 @@ function G = getGains(P,G)
     
     damping_altitude_pitch = 4;
     
-    G.k_i_pitch_altitude = bandwidth_altitude_pitch^2/(k_theta_DC*G.Va_trim)
-    G.k_p_pitch_altitude = 2*damping_altitude_pitch*bandwidth_altitude_pitch/(k_theta_DC*G.Va_trim)
+    G.k_i_pitch_altitude = bandwidth_altitude_pitch^2/(k_theta_DC*G.Va_trim);
+    G.k_p_pitch_altitude = 2*damping_altitude_pitch*bandwidth_altitude_pitch/(k_theta_DC*G.Va_trim);
     
      %% Airspedd from pitch
     
@@ -78,8 +75,8 @@ function G = getGains(P,G)
     
     damping_airspeed_pitch = 1.6;
     
-    G.k_p_pitch_airspeed = (G.a_V1-2*damping_airspeed_pitch*bandwidth_airspeed_pitch)/(k_theta_DC*P.gravity)
-    G.k_i_pitch_airspeed = -bandwidth_airspeed_pitch^2/(k_theta_DC*P.gravity)
+    G.k_p_pitch_airspeed = (G.a_V1-2*damping_airspeed_pitch*bandwidth_airspeed_pitch)/(k_theta_DC*P.gravity);
+    G.k_i_pitch_airspeed = -bandwidth_airspeed_pitch^2/(k_theta_DC*P.gravity);
     
     
     %% Airspedd from throttle
@@ -87,8 +84,8 @@ function G = getGains(P,G)
     bandwidth_airspeed_throttle = 0.7863;
     damping_airspeed_throttle = 1.3;
     
-    G.k_p_throttle_airspeed = (-G.a_V1+2*damping_airspeed_throttle*bandwidth_airspeed_throttle)/(G.a_V2)
-    G.k_i_throttle_airspeed = bandwidth_airspeed_throttle^2/(G.a_V2)
+    G.k_p_throttle_airspeed = (-G.a_V1+2*damping_airspeed_throttle*bandwidth_airspeed_throttle)/(G.a_V2);
+    G.k_i_throttle_airspeed = bandwidth_airspeed_throttle^2/(G.a_V2);
     
 end 
 
