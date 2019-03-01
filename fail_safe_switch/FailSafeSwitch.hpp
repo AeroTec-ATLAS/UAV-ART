8998537813a5fb72d50ed2e327f258c88e49cd0d
joseafonsoneves <joseafonsoneves00@gmail.com>
2019-03-01 23:55:58 +0000

    laranjas

--- fail_safe_switch/FailSafeSwitch.hpp
+++ fail_safe_switch/FailSafeSwitch.hpp
@@ -3,47 +3,92 @@
 
 #include <stdint.h>
 #include <stdio.h>
-#include "GPIO.hpp"
+#include <sys/time.h>  // Add -lrt to compilation command
+#include <wiringPi.h>  // Add -lwiringPi to compilation command
 
-#define RECEIVER_GPIO 1
-#define NOISE_LIMIT 3
+#define RECEIVER_PIN 22
+#define TRI_STATE_PIN 29
+#define NOISE_LIMIT 15
 
 #define PPM_LH_BARRIER 1500
+
+typedef struct timeval TRANSITION_TIME;
 
 class FailSafeSwitch {
  public:
   FailSafeSwitch() {
-    isPilotCommanding = false;  // TO DO como deve ser inicializado
+    hasStarted = false;
+
+    isPilotCommanding = true;  // TO DO como deve ser inicializado
     numEqualSignals = 0;
 
-    receiverGPIO = GPIO(RECEIVER_GPIO);
+    wiringPiSetup();
+
+    pinMode(RECEIVER_PIN, INPUT);
+    pinMode(TRI_STATE_PIN, OUTPUT);
   }
 
-  void processAndFilter(uint16_t period) {
-    // Returns true if the command must change and false otherwise
+  void receiveAndSwitch() {
+    updateSignalStates();
 
-    bool newCommand;
+    if (currState) {
+      if (lastState == 0)
+        // If it receives a HIGH and received a LOW, a positive transition
+        // occurred and time is registered
+        gettimeofday(&lastTransition, NULL);
+    } else if (lastState == 1) {
+      // If it receives a LOW and received a HIGH, a negative transition
+      // occurred
+      bool newCommand = getCommand();
 
-    newCommand = period >= PPM_LH_BARRIER;
+      // If the new signal imposes a change
+      if (newCommand != isPilotCommanding)
+        numEqualSignals++;
+      else
+        numEqualSignals = 0;
 
-    if (newCommand != isPilotCommanding)
-      numEqualSignals++;
-    else
-      numEqualSignals = 0;
+      // If the new signal is confirmed via repetition of signals
+      if (numEqualSignals == NOISE_LIMIT) {
+        isPilotCommanding = newCommand;
+        numEqualSignals = 0;
 
-    if (numEqualSignals == NOISE_LIMIT) {
-      isPilotCommanding = newCommand;
-      numEqualSignals = 0;
-
-      printf("Changed!");
+        digitalWrite(TRI_STATE_PIN, newCommand);
+      }
     }
   }
 
  private:
+  bool hasStarted;
+
+  bool lastState;
+  bool currState;
+
+  TRANSITION_TIME lastTransition;
+
   uint8_t numEqualSignals;
   bool isPilotCommanding;
 
-  GPIO receiverGPIO;
+  void updateSignalStates() {
+    if (!hasStarted) {
+      lastState = digitalRead(RECEIVER_PIN);
+      currState = digitalRead(RECEIVER_PIN);
+    } else {
+      lastState = currState;
+      currState = digitalRead(RECEIVER_PIN);
+    }
+  }
+
+  bool getCommand() {
+    TRANSITION_TIME curr;
+    uint32_t period;
+    bool newCommand;
+
+    gettimeofday(&curr, NULL);
+
+    period = (curr.tv_sec - lastTransition.tv_sec) * 10e6 + curr.tv_usec -
+             lastTransition.tv_usec;
+    newCommand = period >= PPM_LH_BARRIER;
+  }
 };
 
 #endif
