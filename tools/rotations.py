1a167684c5a34770dfc26acbd0d1321450c3938e
afonsoVale <afonsovale007@gmail.com>
2020-04-18 17:39:53 +0100

    Changed sim time to 20s; Added Euler2Rotation

--- tools/rotations.py
+++ tools/rotations.py
@@ -1,6 +1,17 @@
 import numpy as np
+from math import sin, cos
 
 
 def Euler2Rotation(phi, theta, psi):
-    R = np.array([[0, 1, 0], [1, 0, 0], [0, 0, 1]])
+    # Transfroms Euler angles into a rotation matrix R, for a body to inertial rotation
+    s_phi = sin(phi)
+    c_phi = cos(phi)
+    s_theta = sin(theta)
+    c_theta = cos(theta)
+    s_psi = sin(psi)
+    c_psi = cos(psi)
+    
+    R = np.array([[c_theta * c_psi, s_phi * s_theta * c_psi - c_phi * s_psi, c_phi * s_theta * c_psi + s_phi * s_psi],
+                  [c_theta * s_psi, s_phi * s_theta * s_psi + c_phi * c_psi, c_phi * s_theta * s_psi - s_phi * c_psi],
+                  [-s_theta, s_phi * c_theta, c_phi * c_theta]])
     return R
