ccf7aa1fb85b6e522f843606a1de0a3bd0e6fc82
afonsoVale <afonsovale007@gmail.com>
2020-04-18 19:50:59 +0100

    Added a model MAV

--- tools/drawing.py
+++ tools/drawing.py
@@ -10,7 +10,8 @@ mavsim_python: drawing tools
 import numpy as np
 import pyqtgraph.opengl as gl
 from tools.rotations import Euler2Rotation
-#from chap11.dubins_parameters import dubinsParameters # Not needed for chapters 2/3/4 
+# from chap11.dubins_parameters import dubinsParameters # Not needed for chapters 2/3/4
+
 
 class drawMav():
     def __init__(self, state, window):
@@ -28,41 +29,74 @@ class drawMav():
         """
         # get points that define the non-rotated, non-translated mav and the mesh colors
         self.mav_points, self.mav_meshColors = self.get_points()
+        self.faces = self.gatherPointNumbers()
 
         mav_position = np.array([[state.pn], [state.pe], [-state.h]])  # NED coordinates
-        # attitude of mav as a rotation matrix R from body to inertial
-        R = Euler2Rotation(state.phi, state.theta, state.psi)
-        # rotate and translate points defining mav
-        rotated_points = self.rotate_points(self.mav_points, R)
-        translated_points = self.translate_points(rotated_points, mav_position)
-        # convert North-East Down to East-North-Up for rendering
-        R = np.array([[0, 1, 0], [1, 0, 0], [0, 0, -1]])
+        # attitude of mav as a rotation matrix from body to inertial
+        rotation = Euler2Rotation(state.phi, state.theta, state.psi)
+        self.mav_body = []
+        for face in self.faces:
+            R = rotation
+            # rotate and translate points defining mav
+            facePoints = np.array([self.mav_points[face[0] - 1],
+                                   self.mav_points[face[1] - 1],
+                                   self.mav_points[face[2] - 1]]).T
+            # scale points for better rendering
+            scale = 50
+            facePoints = scale * facePoints
+            rotated_points = self.rotate_points(facePoints, R)
+            translated_points = self.translate_points(rotated_points, mav_position)
+            # convert North-East Down to East-North-Up for rendering
+            R = np.array([[0, 1, 0], [1, 0, 0], [0, 0, -1]])
 
-        translated_points = R @ translated_points
-        # convert points to triangular mesh defined as array of three 3D points (Nx3x3)
-        mesh = self.points_to_mesh(translated_points)
-        self.mav_body = gl.GLMeshItem(vertexes=mesh,  # defines the triangular mesh (Nx3x3)
-                                      vertexColors=self.mav_meshColors,  # defines mesh colors (Nx1)
-                                      drawEdges=True,  # draw edges between mesh elements
-                                      smooth=False,  # speeds up rendering
-                                      computeNormals=False)  # speeds up rendering
-        window.addItem(self.mav_body)  # add body to plot
+            translated_points = R @ translated_points
+            # convert points to triangular mesh defined as array of three 3D points (Nx3x3)
+            mesh = self.points_to_mesh(translated_points)
+            if self.faces.index(face) < 8:
+                i = 0  # Yellow main body
+            elif self.faces.index(face) < 10:
+                i = 1  # Blue wing
+            elif self.faces.index(face) < 12:
+                i = 2  # Green horizontal stabilizer
+            else:
+                i = 3  # Red vertical stabilizer
+            self.mav_body.append(gl.GLMeshItem(vertexes=mesh,  # defines the triangular mesh (Nx3x3)
+                                               vertexColors=self.mav_meshColors[i],  # defines mesh colors (Nx1)
+                                               drawEdges=True,  # draw edges between mesh elements
+                                               smooth=False,  # speeds up rendering
+                                               computeNormals=False))  # speeds up rendering
+            window.addItem(self.mav_body[-1])  # add body to plot
 
     def update(self, state):
         mav_position = np.array([[state.pn], [state.pe], [-state.h]])  # NED coordinates
-        # attitude of mav as a rotation matrix R from body to inertial
-        R = Euler2Rotation(state.phi, state.theta, state.psi)
-        # rotate and translate points defining mav
-        rotated_points = self.rotate_points(self.mav_points, R)
-        translated_points = self.translate_points(rotated_points, mav_position)
-        # convert North-East Down to East-North-Up for rendering
-        R = np.array([[0, 1, 0], [1, 0, 0], [0, 0, -1]])
+        # attitude of mav as a rotation matrix from body to inertial
+        rotation = Euler2Rotation(state.phi, state.theta, state.psi)
+        for face in self.faces:
+            # rotate and translate points defining mav
+            R = rotation
+            facePoints = np.array([self.mav_points[face[0] - 1],
+                                   self.mav_points[face[1] - 1],
+                                   self.mav_points[face[2] - 1]]).T
+            # scale points for better rendering
+            scale = 50
+            facePoints = scale * facePoints
+            rotated_points = self.rotate_points(facePoints, R)
+            translated_points = self.translate_points(rotated_points, mav_position)
+            # convert North-East Down to East-North-Up for rendering
+            R = np.array([[0, 1, 0], [1, 0, 0], [0, 0, -1]])
 
-        translated_points = R @ translated_points
-        # convert points to triangular mesh defined as array of three 3D points (Nx3x3)
-        mesh = self.points_to_mesh(translated_points)
-        # draw MAV by resetting mesh using rotated and translated points
-        self.mav_body.setMeshData(vertexes=mesh, vertexColors=self.mav_meshColors)
+            translated_points = R @ translated_points
+            # convert points to triangular mesh defined as array of three 3D points (Nx3x3)
+            mesh = self.points_to_mesh(translated_points)
+            if self.faces.index(face) < 8:
+                i = 0  # Yellow main body
+            elif self.faces.index(face) < 10:
+                i = 1  # Blue wing
+            elif self.faces.index(face) < 12:
+                i = 2  # Green horizontal stabilizer
+            else:
+                i = 3  # Red vertical stabilizer
+            self.mav_body[self.faces.index(face)].setMeshData(vertexes=mesh, vertexColors=self.mav_meshColors[i])
 
     def rotate_points(self, points, R):
         "Rotate points by the rotation matrix R"
@@ -94,10 +128,23 @@ class drawMav():
 
         # points are in NED coordinates
         #   define the points on the aircraft following diagram Fig 2.14
-        points = np.array([[0, 0, 0],  # point 1 [0]
-                           [1, 1, 1],  # point 2 [1]
-                           [1, 1, 0],  # point 3 [2]
-                           ]).T
+        points = [[fuse_l1, 0, 0],  # point 1 [0]
+                  [fuse_l2, fuse_w / 2, -fuse_h / 2],  # point 2 [1]
+                  [fuse_l2, -fuse_w / 2, -fuse_h / 2],  # point 3 [2]
+                  [fuse_l2, -fuse_w / 2, fuse_h / 2],  # point 4 [3]
+                  [fuse_l2, fuse_w / 2, fuse_h / 2],  # point 5 [4]
+                  [-fuse_l3, 0, 0],  # point 6 [5]
+                  [0, wing_w / 2, 0],  # point 7 [6]
+                  [-wing_l, wing_w / 2, 0],  # point 8 [7]
+                  [-wing_l, -wing_w / 2, 0],  # point 9 [8]
+                  [0, -wing_w / 2, 0],  # point 10 [9]
+                  [-(fuse_l3 - tail_l), tail_w / 2, 0],  # point 11 [10]
+                  [-fuse_l3, tail_w / 2, 0],  # point 12 [11]
+                  [-fuse_l3, -tail_w / 2, 0],  # point 13 [12]
+                  [-(fuse_l3 - tail_l), -tail_w / 2, 0],  # point 14 [12]
+                  [-(fuse_l3 - tail_l), 0, 0],  # point 15 [14]
+                  [-fuse_l3, 0, -tail_h],  # point 16 [15]
+                  ]
 
         # scale points for better rendering
         scale = 50
@@ -110,6 +157,9 @@ class drawMav():
         yellow = np.array([1., 1., 0., 1])
         meshColors = np.empty((13, 3, 4), dtype=np.float32)
         meshColors[0] = yellow
+        meshColors[1] = blue
+        meshColors[2] = green
+        meshColors[3] = red
         return points, meshColors
 
     def points_to_mesh(self, points):
@@ -123,6 +173,15 @@ class drawMav():
                          ])
         return mesh
 
+    def gatherPointNumbers(self):
+        # Define the faces that make up the aircraft following diagram 2.14
+        faces = [[1, 2, 3], [1, 3, 4], [1, 2, 5], [1, 5, 4],  # Nose
+                 [3, 4, 6], [4, 5, 6], [2, 3, 6], [2, 5, 6],  # Main frame
+                 [7, 8, 9], [7, 9, 10],  # Wing faces
+                 [11, 12, 13], [11, 13, 14],  # Horizontal stabilizer
+                 [6, 15, 16]]  # Vertical stabilizer
+        return faces
+
 
 class drawPath():
     def __init__(self, path, color, window):
@@ -133,11 +192,11 @@ class drawPath():
         elif path.type == 'orbit':
             points = self.orbit_points(path)
         path_color = np.tile(color, (points.shape[0], 1))
-        self.path_plot_object =  gl.GLLinePlotItem(pos=points,
-                                                   color=path_color,
-                                                   width=2,
-                                                   antialias=True,
-                                                   mode='line_strip')
+        self.path_plot_object = gl.GLLinePlotItem(pos=points,
+                                                  color=path_color,
+                                                  width=2,
+                                                  antialias=True,
+                                                  mode='line_strip')
         window.addItem(self.path_plot_object)
 
     def update(self, path):
@@ -160,12 +219,11 @@ class drawPath():
         points = points @ R.T
         return points
 
-
     def orbit_points(self, path):
         N = 10
         theta = 0
         theta_list = [theta]
-        while theta < 2*np.pi:
+        while theta < 2 * np.pi:
             theta += 0.1
             theta_list.append(theta)
         points = np.array([[path.orbit_center.item(0) + path.orbit_radius,
@@ -186,9 +244,9 @@ class drawWaypoints():
     def __init__(self, waypoints, radius, color, window):
         self.radius = radius
         self.color = color
-        if waypoints.type=='straight_line' or waypoints.type=='fillet':
+        if waypoints.type == 'straight_line' or waypoints.type == 'fillet':
             points = self.straight_waypoint_points(waypoints)
-        elif waypoints.type=='dubins':
+        elif waypoints.type == 'dubins':
             points = self.dubins_points(waypoints, self.radius, 0.1)
         waypoint_color = np.tile(color, (points.shape[0], 1))
         self.waypoint_plot_object = gl.GLLinePlotItem(pos=points,
@@ -199,9 +257,9 @@ class drawWaypoints():
         window.addItem(self.waypoint_plot_object)
 
     def update(self, waypoints):
-        if waypoints.type=='straight_line' or waypoints.type=='fillet':
+        if waypoints.type == 'straight_line' or waypoints.type == 'fillet':
             points = self.straight_waypoint_points(waypoints)
-        elif waypoints.type=='dubins':
+        elif waypoints.type == 'dubins':
             points = self.dubins_points(waypoints, self.radius, 0.1)
         self.waypoint_plot_object.setData(pos=points)
 
@@ -214,12 +272,12 @@ class drawWaypoints():
         # returns a list of points along the dubins path
         initialize_points = True
         dubins_path = dubinsParameters()
-        for j in range(0, waypoints.num_waypoints-1):
+        for j in range(0, waypoints.num_waypoints - 1):
             dubins_path.update(
-                waypoints.ned[:, j:j+1],
+                waypoints.ned[:, j:j + 1],
                 waypoints.course.item(j),
-                waypoints.ned[:, j+1:j+2],
-                waypoints.course.item(j+1),
+                waypoints.ned[:, j + 1:j + 2],
+                waypoints.course.item(j + 1),
                 radius)
 
             # points along start circle
@@ -233,7 +291,7 @@ class drawWaypoints():
             theta_list = [th]
             if dubins_path.dir_s > 0:
                 if th1 >= th2:
-                    while th < th2 + 2*np.pi:
+                    while th < th2 + 2 * np.pi:
                         th += Del
                         theta_list.append(th)
                 else:
@@ -242,7 +300,7 @@ class drawWaypoints():
                         theta_list.append(th)
             else:
                 if th1 <= th2:
-                    while th > th2 - 2*np.pi:
+                    while th > th2 - 2 * np.pi:
                         th -= Del
                         theta_list.append(th)
                 else:
@@ -310,10 +368,11 @@ class drawWaypoints():
     def mod(self, x):
         # force x to be between 0 and 2*pi
         while x < 0:
-            x += 2*np.pi
-        while x > 2*np.pi:
-            x -= 2*np.pi
+            x += 2 * np.pi
+        while x > 2 * np.pi:
+            x -= 2 * np.pi
         return x
+
 
 class drawMap():
     def __init__(self, map, window):
@@ -390,5 +449,3 @@ class drawMap():
         meshColors[8] = yellow
         meshColors[9] = yellow
         return mesh, meshColors
-
-
--- tools/rotations.py
+++ tools/rotations.py
@@ -10,7 +10,7 @@ def Euler2Rotation(phi, theta, psi):
     c_theta = cos(theta)
     s_psi = sin(psi)
     c_psi = cos(psi)
-    
+
     R = np.array([[c_theta * c_psi, s_phi * s_theta * c_psi - c_phi * s_psi, c_phi * s_theta * c_psi + s_phi * s_psi],
                   [c_theta * s_psi, s_phi * s_theta * s_psi + c_phi * c_psi, c_phi * s_theta * s_psi - s_phi * c_psi],
                   [-s_theta, s_phi * c_theta, c_phi * c_theta]])
