2c30237734d705b7eff939b1dc6e8f2dce253624
afonsoVale <afonsovale007@gmail.com>
2020-04-19 20:29:06 +0100

    Added Quaternion2Euler and Quaternion2Rotation for redundancy

--- tools/rotations.py
+++ tools/rotations.py
@@ -1,5 +1,5 @@
 import numpy as np
-from math import sin, cos
+from math import sin, cos, atan2, asin
 
 
 def Euler2Rotation(phi, theta, psi):
@@ -24,3 +24,16 @@ def Euler2Quaternion(phi, theta, psi):
     e3 = sin(psi / 2) * cos(theta / 2) * cos(phi / 2) + cos(psi / 2) * sin(theta / 2) * sin(phi / 2)
     e = np.array([e0, e1, e2, e3])
     return e
+
+
+def Quaternion2Euler(e0, e1, e2, e3):
+    phi = atan2(2(e0 * e1 + e2 * e3), e0**2 + e3**2 - e1**2 - e2**2)
+    theta = asin(2(e0 * e2 - e1 * e3))
+    psi = atan2(2(e0 * e3 + e1 * e2), e0**2 + e1**2 - e2**2 - e3**2)
+    return phi, theta, psi
+
+
+def Quaternion2Rotation(e0, e1, e2, e3):
+    phi, theta, psi = Quaternion2Euler(e0, e1, e2, e3)
+    R = Euler2Rotation(phi, theta, psi)
+    return R
