4850c644d41344562537ade69c5341a2fbd9731f
afonsoVale <afonsovale007@gmail.com>
2020-04-25 15:30:42 +0100

    Added support for joystick input for testing; Fixed camera position behind MAV

--- chap2/mav_viewer.py
+++ chap2/mav_viewer.py
@@ -21,10 +21,10 @@ class mavViewer():
         self.window.setWindowTitle('MAV Viewer')
         self.window.setGeometry(0, 0, 1000, 1000)  # args: upper_left_x, upper_right_y, width, height
         grid = gl.GLGridItem()  # make a grid to represent the ground
-        grid.setSize(100,100,100) # set the span of the grid 
+        grid.setSize(500,500,500) # set the span of the grid 
         grid.scale(20, 20, 20)  # set the size of the grid (distance between each line)
         self.window.addItem(grid)  # add grid to viewer
-        self.window.setCameraPosition(distance=200) # distance from center of plot to camera
+        self.window.setCameraPosition(distance=150) # distance from center of plot to camera
         self.window.setBackgroundColor('k')  # set background color to black
         self.window.show()  # display configured window
         self.window.raise_()  # bring window to the front
@@ -42,5 +42,6 @@ class mavViewer():
         # update the center of the camera view to the mav location
         view_location = Vector(state.pe, state.pn, state.h)  # defined in ENU coordinates
         self.window.opts['center'] = view_location
+        self.window.setCameraPosition(azimuth=-np.degrees(state.psi+state.beta)-90,elevation=-np.degrees(state.theta)+10)
         # redraw
         self.app.processEvents()
---
+++ chap4/mavsim_chap4_joystick.py
@@ -0,0 +1,93 @@
+"""
+mavsimPy
+    - Chapter 4 assignment for Beard & McLain, PUP, 2012
+    - Update history:  
+        12/27/2018 - RWB
+        1/17/2019 - RWB
+        2/24/2020 - RWB
+"""
+import sys
+sys.path.append('..')
+import numpy as np
+import parameters.simulation_parameters as SIM
+
+from chap2.mav_viewer import mavViewer
+from chap3.data_viewer import dataViewer
+from chap4.mav_dynamics import mavDynamics
+from chap4.wind_simulation import windSimulation
+from tools.log import log
+import pygame
+
+pygame.display.init()
+pygame.joystick.init()
+sideStick = pygame.joystick.Joystick(1)
+sideStick.init()
+thrustLever = pygame.joystick.Joystick(2)
+thrustLever.init()
+rudder = pygame.joystick.Joystick(4)
+rudder.init()
+
+# initialize the visualization
+VIDEO = False  # True==write video, False==don't write video
+mav_view = mavViewer()  # initialize the mav viewer
+data_view = dataViewer()  # initialize view of data plots
+logFile = log('dados_log.txt')
+if VIDEO is True:
+    from chap2.video_writer import videoWriter
+    video = videoWriter(video_name="chap4_video.avi",
+                        bounding_box=(0, 0, 1000, 1000),
+                        output_rate=SIM.ts_video)
+
+# initialize elements of the architecture
+wind = windSimulation(SIM.ts_simulation)
+wind._steady_state = np.array([[5., 2., 0.]]).T  # Steady wind in NED frame
+mav = mavDynamics(SIM.ts_simulation)
+
+# initialize the simulation time
+sim_time = SIM.start_time
+plot_time = sim_time
+
+delta_e = 0.
+delta_a = 0.
+delta_r = 0.
+delta_t = 0.
+
+# main simulation loop
+while sim_time < SIM.end_time:
+    pygame.event.pump()
+    delta_e = -sideStick.get_axis(1) * np.radians(45/2)
+    delta_a = sideStick.get_axis(0) * np.radians(45/2)
+    delta_r = -rudder.get_axis(2) * np.radians(45/2)
+    delta_t = (thrustLever.get_axis(4) + 1) / 2
+    # -------set control surfaces-------------
+    #delta_e += 0.001 * (np.random.random() - 0.5)
+    #delta_a += 0.001 * (np.random.random() - 0.5)
+    #delta_r += 0.001 * (np.random.random() - 0.5)
+    #delta_t += 0.001 * (np.random.random() - 0.5)
+    delta = np.array([[delta_e, delta_a, delta_r, delta_t]]).T
+    # transpose to make it a column vector
+
+    # -------physical system-------------
+    current_wind = wind.update()  # get the new wind vector
+    mav.update(delta, current_wind)  # propagate the MAV dynamics
+
+    # -------update viewer-------------
+    if sim_time - plot_time > SIM.ts_plotting:
+        mav_view.update(mav.true_state)  # plot body of MAV
+        plot_time = sim_time
+    data_view.update(mav.true_state,  # true states
+                     mav.true_state,  # estimated states
+                     mav.true_state,  # commanded states
+                     SIM.ts_simulation)
+    logFile.addEntry(mav.true_state, delta, sim_time)
+    if VIDEO is True:
+        video.update(sim_time)
+
+    # -------increment time-------------
+    sim_time += SIM.ts_simulation
+    if mav.true_state.h <= 0:  # Touches the ground
+        break
+logFile.closeLog()
+input("Press any key to exit...")
+if VIDEO is True:
+    video.close()
