4710ed56b5f47c284c1b0dc633bec8548bc380f5
afonsoVale <afonsovale007@gmail.com>
2020-09-29 19:03:20 +0100

    Added vision files

---
+++ vision/SIFT_VIDEO_POST.py
@@ -0,0 +1,183 @@
+import cv2
+import numpy as np
+import random
+from operator import itemgetter
+
+initial_state=0
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+colors = [(0, 0, 255), (255, 0, 255), (255, 0, 145), (255, 0, 0), (0, 255, 0), (255, 255, 0)]
+
+names = ['/Users/arthurlago/Documents/UAV/CV/Videos-Post-Insta/IMG_4121.MOV', '/Users/arthurlago/Documents/UAV/CV/Videos-Post-Insta/IMG_4122.MOV']
+
+cap = [cv2.VideoCapture(i) for i in names]
+
+frames = [None] * len(names);
+ret = [None] * len(names);
+
+while True:
+
+    for i,c in enumerate(cap):
+        if c is not None:
+            ret[i], frames[i] = c.read();
+
+
+    for i,f in enumerate(frames):
+        if ret[i] is True:
+            sift = cv2.xfeatures2d.SIFT_create()
+            kp1, d1 = sift.detectAndCompute(frames[0],None)
+            kp2, d2 = sift.detectAndCompute(frames[1],None)
+
+            ####### MATCHING ##########
+            matcher = cv2.DescriptorMatcher_create(cv2.DescriptorMatcher_FLANNBASED)
+            if initial_state==1:
+                firstd1 = np.array(firstd1, dtype=np.float32)
+                knn_matches1 = matcher.knnMatch(firstd1, d1, 2)
+
+                ratio_thresh = 0.7
+                good1 = []
+                for m,n in knn_matches1:
+                    if m.distance < ratio_thresh * n.distance:
+                        good1.append(m)
+                # print(good1)
+
+                i = 0
+                newkeypoints1 = []
+                newd1 = []
+                novalista = []
+                # print(len(good1))
+                while i < len(good1):
+
+                    numero = good1[i].trainIdx
+                    newkp = kp1[numero]
+                    newkeypoints1.append(newkp)
+                    desc = d1[numero]
+                    newd1.append(desc)
+                    novalista.append((numero, desc))
+
+                    i=i+1
+
+
+                # print(novalista)
+
+
+            if initial_state==0:
+                knn_matches2 = matcher.knnMatch(d1, d2, 2)
+            if initial_state==1:
+               newd1 = np.array(newd1, dtype=np.float32)
+               knn_matches2 = matcher.knnMatch(newd1, d2, 2)
+            # if initial_state==1:q
+            if initial_state==0:
+                ratio_thresh = 0.4
+            if initial_state==1:
+                ratio_thresh = 0.7
+            good2 = []
+            for m,n in knn_matches2:
+                if m.distance < ratio_thresh * n.distance:
+                    good2.append(m)
+
+            if initial_state==0:
+                good2 = good2[:6]
+            # print(good2)
+
+            img3 = np.hstack((frames[0], frames[1]))
+
+            i = 0
+            if initial_state == 0:
+                newkeypointsK = []
+            newkeypoints2 = []
+            if initial_state==1:
+                newkeypointsK = []
+                newlistacor = []
+            if initial_state == 0:
+                firstd1 = []
+                listacor = []
+            keypoint = 0
+            while i < len(good2):
+                if initial_state == 0:
+                    keypoint3 = good2[i].queryIdx
+                    keypoint1 = kp1[keypoint3]
+                    newdescriptors = d1[keypoint3]
+                    firstd1.append(newdescriptors)
+                    listacor.append((newdescriptors, i))
+                    newkeypointsK.append(keypoint1)
+                if initial_state == 1:
+                    p = 0
+                    keypoint3 = good2[i].queryIdx
+                    while p<len(good1):
+                        new = good1[p].trainIdx
+                        new1 = d1[new]
+                        # print('\n')
+                        z = 0
+                        while z<len(good2):
+                            keypoint = good2[z].queryIdx
+                            new2 = newd1[keypoint]
+                            # print('\n\n\n\n')
+                            comparison1 = new1 == new2
+                            equal = comparison1.all()
+
+                            if equal==1:
+                                print('passou')
+                                aaa = good1[p].queryIdx
+                                aaa2 = listacor[aaa][1]
+                                newlistacor.append(aaa2)
+                            z=z+1
+                        p=p+1
+
+                    keypoint1 = newd1[keypoint3]
+                    x=0
+                    while x<len(novalista):
+                        # print(keypoint1)
+                        # print(novalista[x][1])
+                        # print("\n\n\n\n")
+                        comparison = novalista[x][1] == keypoint1
+                        equal_arrays = comparison.all()
+                        if equal_arrays==1:
+                            # print('passou')
+                            keypoint = novalista[x][0]
+                            keypoint1 = kp1[keypoint]
+                            newkeypointsK.append(keypoint1)
+                        x = x+1
+                keypoint = good2[i].trainIdx
+                keypoint2 = kp2[keypoint]
+                newkeypoints2.append(keypoint2)
+                i=i+1
+
+            # print(firstd1)
+
+            # print(len(good2))
+            # print(len(newkeypoints1))
+            if initial_state==1:
+                print(newlistacor)
+            # print('passou')
+            i=0
+            while i <len(newkeypointsK) and i<len(newkeypoints2):
+                # cor1 = random.randint(0, 255)
+                if initial_state==1:
+                    aaaaa = newlistacor[i]
+                if initial_state==0:
+                    aaaaa = i
+                frames[0]= cv2.circle(frames[0], (int(newkeypointsK[i].pt[0]), int(newkeypointsK[i].pt[1])), 30, colors[aaaaa], 20)
+                # cv2.putText(frames[0], (i+1), (int(newkeypointsK[i].pt[0])+20, int(newkeypointsK[i].pt[1])+20), font, 4, (255, 255, 255), 1, cv2.LINE_AA)
+                frames[1] = cv2.circle(frames[1], (int(newkeypoints2[i].pt[0]), int(newkeypoints2[i].pt[1])), 30, colors[aaaaa], 20)
+                # cv2.putText(frames[1], (i+1), (int(newkeypoints2[i].pt[0])+20, int(newkeypoints2[i].pt[1])+20), font, 1, (255, 255, 255), 1, cv2.LINE_AA)
+                print(i)
+                i=i+1
+
+
+            # print(initial_state)
+            img3 = np.hstack((frames[0], frames[1]))
+            cv2.namedWindow('Frame', cv2.WINDOW_GUI_NORMAL)
+            cv2.imshow('Frame',img3)
+
+            initial_state = 1
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+       break
+
+
+for c in cap:
+    if c is not None:
+        c.release();
+
+cv2.destroyAllWindows()
---
+++ vision/SIFT_VIDEO_TRACKING.py
@@ -0,0 +1,231 @@
+import cv2
+import numpy as np
+import argparse
+import math
+import matplotlib.pyplot as plt
+from operator import itemgetter
+##################### INITIAL CONDITIONS ###################################
+clickcon = 0
+global mouseX,mouseY
+global initial_state
+global initial_state2
+global newd1
+initial_state = 1
+initial_state2 = 1
+
+
+def distance(x0,y0,x1,y1):
+    r = math.sqrt( ((x1-x0)**2)+((y1-y0)**2) )
+    return r
+
+def selection_sort(x):
+    for i in range(len(x)):
+        swap = i + np.argmin(x[i:])
+        (x[i], x[swap]) = (x[swap], x[i])
+    return x
+
+
+
+
+par = 0
+
+##################### VIDEO ################################################
+# cap = cv2.VideoCapture('/Users/arthurlago/Documents/UAV/CV/Videos/Pexels Videos 1570894.mp4')
+cap = cv2.VideoCapture(0)
+
+descriptorlist = []
+CONDITION1=0
+distlist = []
+
+while(cap.isOpened()):
+
+  ret, frame = cap.read()
+  if ret == True:
+#################### MOUSE CLICK #########################################
+      # width = int(frame.shape[1] * 0.3)
+      # height = int(frame.shape[0] * 0.3)
+      #
+      # frame = cv2.resize(frame, (width, height))
+      def click_event(event, x, y, flags, param):
+          global mouseX,mouseY
+          global initial_state
+          global initial_state2
+          if event == cv2.EVENT_LBUTTONDOWN:
+              mouseX,mouseY = x,y
+              #print(mouseX, ' ', mouseY)
+              initial_state2 = 0
+              initial_state = 0
+          if event == cv2.EVENT_RBUTTONDOWN:
+              initial_state = 1 #Reinicia os keypoints
+
+################ DETECTOR #######################################################
+      # print(initial_state2)
+      # print(initial_state)
+      if initial_state == 0:
+        print(mouseX, mouseY)
+      sift = cv2.xfeatures2d.SIFT_create()
+      kp1, d1 = sift.detectAndCompute(frame,None)
+      u = len(kp1)
+      poskp1 = []
+      z=0
+
+      while z < u:
+          # print (kp1[x].pt)
+          poskp1.append([kp1[z].pt])
+          z=z+1
+
+############## DISTANCE KP TO MOUSE ##################################################
+
+      if initial_state2 == 0:
+          kp_dist = []
+          counter = 0
+          for element in kp1:
+            dist = math.sqrt( ((float(mouseX)-element.pt[0])**2)+((float(mouseY)-element.pt[1])**2) )
+            kp_dist.append([dist, counter])
+            counter = counter + 1
+
+          number_kp = 25
+          kp_dist = np.array(kp_dist)
+          # smallest_index = np.argsort(kp_dist)[:number_kp]
+          newkp1 = []
+          #
+          # print('Smallest: ',smallest_index)
+          # newkp1 =[]
+          # for index1 in smallest_index:
+          #   print(kp_dist[index1])
+          #   newkp1.append(kp1[index1])
+          kp_dist = sorted(kp_dist, key=itemgetter(0))
+          kp_dist = kp_dist[:number_kp]
+          # print(kp_dist)
+          # print(kp_dist)
+          # print('\n\n\n')
+          counter = 0
+          newd1 = []
+
+          while counter < len(kp_dist):
+              numero = int(kp_dist[counter][1])  ###ASSIM QUE SE CHEGA AO NUMERO DA ITERAÇÃO
+              # print(kp_dist[counter][1])
+              counter = counter + 1
+              desc = d1[numero]
+              newd1.append(desc)
+
+          # print(newd1)
+          # print(d1)
+
+          # smallest_index = kp_dist[:number_kp]
+          # counter = 0
+          # while counter < number_kp:
+          #     newkp1.append([kp1[smallest_index[counter][1]]])
+
+            # print(kp_dist[index1])
+
+
+          #plt.hist(kp_dist, bins = 10)
+          #plt.show()
+
+          # frame = cv2.drawKeypoints(frame,newkp1,None,flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+      if (initial_state)!= 0:
+        # frame = cv2.drawKeypoints(frame, kp1, None)
+        frame = cv2.drawKeypoints(frame,kp1,None,flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+
+################ MATCHER #######################################################
+      # if initial_state == 0:
+      #     counter1=0
+      #     newd1 = newd1
+      #     # print(newd1)
+      #     eudistlist = []
+      #     newlst = []
+      #     while counter1 < len(newd1):
+      #         counter=0
+      #         while counter < len(d1):
+      #             eudist  = np.linalg.norm(newd1[counter1]-d1[counter],ord=None, axis=None, keepdims=False)
+      #             eudistlist.append([eudist, counter, counter1])
+      #             newlst[counter1].append([eudist, counter])
+      #             counter = counter+1
+      #         counter1 = counter1 + 1
+      #
+      #     print(eudistlist)
+
+
+
+######
+
+      if initial_state == 0 and initial_state2!=0:
+
+          newd1 = np.array(newd1, dtype=np.float32)
+          print(newd1)
+          # print(len(d1))
+          matcher = cv2.DescriptorMatcher_create(cv2.DescriptorMatcher_FLANNBASED)
+          knn_matches = matcher.knnMatch(newd1, d1, 2)
+          # print(eudistlist)
+          # print(len(knn_matches))
+
+          ratio_thresh = 0.65
+          good = []
+          dist = []
+          for m,n in knn_matches:
+              if m.distance < ratio_thresh * n.distance:
+                  good.append(m)
+
+          i = 0
+          newkeypoints1 = []
+          while i < len(good):
+              keypoint = good[i].trainIdx
+              keypoint1 = kp1[keypoint]
+              newkeypoints1.append(keypoint1)
+              # print(newkeypoints1[i].size)
+              i = i+1
+
+
+
+          i = 0
+          while i < len(newkeypoints1):
+              # cv2.putText(frame,"x", (int(newkeypoints1[i].pt[0]), int(newkeypoints1[i].pt[1])), cv2.FONT_HERSHEY_SIMPLEX, 1, 255)
+              frame = cv2.circle(frame, (int(newkeypoints1[i].pt[0]), int(newkeypoints1[i].pt[1])), int(newkeypoints1[i].size), (0,144,255), 2)
+              i = i+1
+          # frame = cv2.drawKeypoints(frame,newkeypoints1,None,flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+
+      #
+      # if CONDITION1 > 0:
+      #     # print ('HEY')
+      #     i = 0
+      #     # print(descriptorlistframeantigo)
+      #     while i <= (len(descriptorlistframeantigo)-1):
+      #         # z = 0
+      #         # while z <= (len(d1)-1):
+      # if initial_state == 0:
+      #     matcher = cv2.DescriptorMatcher_create(cv2.DescriptorMatcher_FLANNBASED)
+      #     knn_matches = matcher.knnMatch(newd1, d1, 2)
+      # #
+      #     print(knn_matches)
+          # print('\n\n\n')
+          # print('NEW FRAME')
+
+
+################ SHOW IMAGES #######################################################
+
+      # width = int(frame.shape[1] * 0.6)
+      # height = int(frame.shape[0] * 0.6)
+
+      # frame = cv2.resize(frame, (width, height))
+      cv2.namedWindow('Frame', cv2.WINDOW_GUI_NORMAL)
+      cv2.imshow('Frame',frame)
+
+      descriptorlist.append(d1)
+      descriptorlistframeantigo = [d1]
+      # print(descriptorlistframeantigo)
+
+      CONDITION1=1
+      # initial_state = 1
+      initial_state2 = 1
+      cv2.setMouseCallback("Frame", click_event)
+      if cv2.waitKey(25) & 0xFF == ord('q'):
+          break
+  else:
+      break
+
+
+cap.release()
