8787a5c13eeea3a5af258eed114cb2ace7698b23
afonsoVale <afonsovale007@gmail.com>
2020-12-08 17:37:26 +0000

    Fixed state variable in raspberry pi file

--- UAV-P2/Raspberry_Pi/main.py
+++ UAV-P2/Raspberry_Pi/main.py
@@ -25,7 +25,7 @@ raspIP='192.168.1.13'
 #sensors=Sensors(localIP, raspIP)
 # initialize the visualization
 #ground=groundProxy()
-servo=servo_stimulation()
+#servo=servo_stimulation()
 # initialize elements of the architecture
 ctrl = autopilot(0.01)
 #obsv = observer(SIM.ts_simulation)
@@ -74,11 +74,11 @@ while True:
     # autopilot_commands = path_follow.update(path, mav.true_state)  # for debugging
 
     # -------controller-------------
-    delta, commanded_state = ctrl.update(autopilot_commands, state, previous_t, sim_time)
+    delta, commanded_state = ctrl.update(autopilot_commands, mav.true_state, previous_t, sim_time)
     previous_t = delta.throttle
-    servo.stimulation(delta.to_array())
+    #servo.stimulation(delta.to_array())
     print(np.degrees(delta.to_array()))
-    logger.addEntry(state, delta, sim_time)
+    logger.addEntry(mav.true_state, delta, sim_time)
     # -------update viewer-------------
     #ground.sendToVisualizer(state, delta)
     # -------physical system-------------
