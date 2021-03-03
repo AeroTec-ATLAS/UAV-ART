88465e9f516c0a6ab20fda6883e8af7a7d44c1ca
Gcarreira99 <47892704+Gcarreira99@users.noreply.github.com>
2021-03-03 00:11:58 +0000

    Update servo_stimulation.py

--- UAV-P2/Raspberry_Pi/control/servo_stimulation.py
+++ UAV-P2/Raspberry_Pi/control/servo_stimulation.py
@@ -1,40 +1,21 @@
 import sys
 sys.path.append('..')
 import numpy as np
-from gpiozero import AngularServo
-from time import sleep
+from adafruit_servokit import ServoKit
 
 class servo_stimulation:
 	
-	porta_de = 17
-	porta_da = 27
-	porta_dr = 22
-	min_de=-45
-	min_da=-60
-	min_dr=-40
-	max_de=45
-	max_da=30
-	max_dr=30
-	prev_de=0
-	prev_da=0
-	prev_dr=0
-	
-	def __init__(self):	
-		self.servo_de = AngularServo(self.porta_de, min_angle=self.min_de, max_angle=self.max_de)
-		self.servo_da = AngularServo(self.porta_da, min_angle=self.min_da, max_angle=self.max_da)
-		self.servo_dr = AngularServo(self.porta_dr, min_angle=self.min_dr, max_angle=self.max_dr)
+	def __init__(self):
+		self.kit = ServoKit(channels=16)	
+		self.servo_de = kit.servo[4]
+		self.servo_da = kit.servo[8]
+		self.servo_dr = kit.servo[12]
 
 
 	def stimulation(self, u):
-		if self.prev_de != u[0]:
-			self.servo_de.angle = np.degrees(u[0])
-			self.prev_de = u[0]
-		if self.prev_da != u[1]:
-			self.servo_da.angle = np.degrees(u[1])
-			self.prev_da = u[1]
-		if self.prev_dr != u[2]:
-			self.servo_dr.angle = np.degrees(u[2])
-			self.prev_da = u[2]
+		self.servo_de.angle = np.degrees(u[0]) + 90
+		self.servo_da.angle = np.degrees(u[1]) + 90
+		self.servo_dr.angle = np.degrees(u[2]) + 90
 
 
 
