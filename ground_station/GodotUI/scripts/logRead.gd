138b54e9b4caa821a337c1f03b92170d83a63ef4
pcaires <josepedro2nd@gmail.com>
2021-04-10 23:52:33 +0100

    Fixed bug with manual camera in no UI mode

--- ground_station/GodotUI/scripts/logRead.gd
+++ ground_station/GodotUI/scripts/logRead.gd
@@ -124,7 +124,7 @@ func _process(delta):
 func _input(event):
 	if Input.is_action_just_pressed("ui_cancel"):
 		if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
-			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+			Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 			$GUI.set_visible(false)
 		else:
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
