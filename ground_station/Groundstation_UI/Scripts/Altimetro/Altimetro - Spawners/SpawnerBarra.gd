20b80b458f623ed8a52ca9f48f63d4bbdc698525
Skarfoul <arthur.lobo@tecnico.ulisboa.pt>
2022-05-26 23:13:35 +0100

    Finished the Altimeter and AS

--- ground_station/Groundstation_UI/Scripts/Altimetro/Altimetro - Spawners/SpawnerBarra.gd
+++ ground_station/Groundstation_UI/Scripts/Altimetro/Altimetro - Spawners/SpawnerBarra.gd
@@ -6,6 +6,7 @@ extends Node2D
 var contBarraM = 1
 var contBarraN = -1
 var estado = 1
+var estadoNeg = 1
 var neg1
 var pos1
 #var barra = preload("res://Scenes/Barra.tscn")
@@ -19,24 +20,15 @@ func _process(delta):
 	#if (float(Debug.x1) >= (27.0 + (49.3597/10.0 * Debug.x))):
 	#print(contBarraM)
 	if(estado == 1):
-		if (float(Debug.x) >= (160.5 * (contBarraM))): #161.164 #por ajuste da alt ini
+		if (float(Debug.x) >= (160.5 * (contBarraM-1))): #161.164 #por ajuste da alt ini
 			#print("criou1")
 			var barra = preload("res://Scenes/Barra.tscn")
 			var barraC = barra.instance()
 			add_child_below_node(get_tree().get_root().get_node("PFD"), barraC)
 			estado = 2
 			contBarraM += 1
-		#if (float(Debug.x) <= (170.5 * (contBarraN))): #161.164 #por ajuste da alt ini
-		#	print("NEGATIVO1")
-		#	var neg1 = 1
-		#	var barra = preload("res://Scenes/Barra.tscn")
-	#		var barraC = barra.instance()
-	#		add_child_below_node(get_tree().get_root().get_node("PFD"), barraC)
-	#		neg1 = 0
-	#		estado = 8
-	#		contBarraN -= 1
 	elif(estado==2):    #272 é isso #||estado==8
-		if (float(Debug.x) >= (136.0 * (contBarraM))): #137 #InitialAlt.pinit * 10 + 10 * (SpawnerNum.contM -3)
+		if (float(Debug.x) >= (136.0 * (contBarraM-1))): #137 #InitialAlt.pinit * 10 + 10 * (SpawnerNum.contM -3)
 			#print("criou2")
 			#print("criou2")
 			#print(contBarraM)
@@ -47,30 +39,10 @@ func _process(delta):
 			contBarraM += 1
 			estado = 3
 	elif(estado==3):
-		if (float(Debug.x) >= (126.0 * (contBarraM))): #113.0
+		if (float(Debug.x) >= (126.0 * (contBarraM-1))): #113.0
 			print("3+")
 			var barra2 = preload("res://Scenes/Barra3.tscn")
 			var barraC2 = barra2.instance()
 			add_child_below_node(get_tree().get_root().get_node("PFD"), barraC2)
 			contBarraM += 1
-		#	if(estado ==3):
-		#		if (float(Debug.x) >= (137 * (contBarraM-1))):
-		#			var barra2 = preload("res://Scenes/Barra2.tscn")
-		#			var barraC2 = barra1.instance()
-		#			add_child_below_node(get_tree().get_root().get_node("PFD"), barraC2)
-		#			contBarraM += 1
-	#else:
-		#if (float(Debug.x) >= ( 137 * (contBarraM-1))): #137
-		#	estado = 4
-			#print("criou3")
-			#print("criou2")
-			#print(contBarraM)
-			#if Input.is_action_just_pressed("ui_accept"):
-		#	var barra2 = preload("res://Scenes/Barra3.tscn")
-			#var barraC2 = barra2.instance()
-			#add_child_below_node(get_tree().get_root().get_node("PFD"), barraC2)
-			#contBarraM += 1
-#func spawn(spawn_type, spawn_position):
-	#var new_spawn = spawn_type.instance()
-	#add_child(new_spawn)
-	#new_spawn.global_position = spawn_position.global_position
+	
---
+++ ground_station/Groundstation_UI/Scripts/Altimetro/BarraGr2.gd
@@ -0,0 +1,5 @@
+extends Sprite
+
+
+func _process(delta):
+	position.y = (1665+(49.3597/10.0 * Debug.x))
