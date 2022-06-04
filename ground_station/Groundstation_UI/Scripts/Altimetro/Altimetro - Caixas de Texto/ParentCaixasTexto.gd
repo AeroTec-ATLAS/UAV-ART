b18724de88988b1d917f4290653ce33efe0d2402
Skarfoul <arthur.lobo@tecnico.ulisboa.pt>
2022-06-04 15:44:36 +0100

    Bug fixes in the altimeter

--- ground_station/Groundstation_UI/Scripts/Altimetro/Altimetro - Caixas de Texto/ParentCaixasTexto.gd
+++ ground_station/Groundstation_UI/Scripts/Altimetro/Altimetro - Caixas de Texto/ParentCaixasTexto.gd
@@ -10,13 +10,14 @@ func _on_Sprite_Leitura(array2, pinit, init):
 	#position.y = 0
 	if estado ==1 :
 		#position.y = -pinit*10 -3.76197*float(Debug.x) + (49.3597/10 * float(Debug.x))
-		position.y = -pinit*10 -3.76197*float(Debug.x) - 90 - 0.173999786*pinit*10  #+ 5 * init #-90
+		position.y = -pinit*10 -3.76197*float(Debug.x) - 93 - 0.173999786*pinit*10 + 3.76199 * init #-90
 		#position.y = -pinit*10 -3.76197*float(Debug.x) - 90 - 0.173999786*pinit*10 FUNCIONA
 		aux = position.y
 		estado +=1
-		#print(pinit)
+		#print(position.y)
+		#print(init)
 	position.y = 49.3597/10 * float(Debug.x) +aux
-	print(position.y)
+	#print(position.y)
 	#print(position.y)
 	#print(Debug.x)
 		#position.y = -pinit*10 -87.0 + (49.3597/10 * float(Debug.x)) #array2[9])
