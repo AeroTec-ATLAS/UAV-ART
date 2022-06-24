ee163c967c782d671abf7ab10b54cc9f231276c9
Skarfoul <arthur.lobo@tecnico.ulisboa.pt>
2022-06-24 13:34:30 +0100

    Better coords convertion

--- ground_station/Groundstation_UI/Viewport.gd
+++ ground_station/Groundstation_UI/Viewport.gd
@@ -49,8 +49,17 @@ func converter(pos_x,pos_y):
 	#novo coord_y pra uma dist maior
 	#coord_y = 39.204206 + 0.00011851699855345 * (3507.0 - pos_y)
 	#coord_x = -8.057659 + 0.00019261856421903 * (4960.0 - pos_x)#(2383.840088 - pos_x)
-	coord_y = 39.292701 + 0.000056330421808076 * (278.132843 - pos_y)
-	coord_x = -8.179168 - 0.000067340310025321 * (533.211487 - pos_x)#(2383.840088 - pos_x)
-	print(coord_y," ",coord_x)
+	#coord_y = 39.292701 + 0.000056330421808076 * (278.132843 - pos_y)
+	#coord_x = -8.179168 - 0.000067340310025321 * (533.211487 - pos_x)#(2383.840088 - pos_x)
+	
 	#39.292701, -8.179168 parte esquerda cruzamento y = 278.132843 x =533.211487
 	#39.245161, -8.030861 rotunda y = 1122.081787 x = 2735.5625
+	#mais novo X:
+	#39.244551, -7.911658 novo 39.251572, -7.929948
+	#coord_x = -8.179169 - 0.000070333891220336 * (533.522217 - pos_x)#(533.211487 - pos_x)
+	#MAIS NOVO Y:
+	#39.148193, -7.995535
+	coord_y = 39.292701 + 0.000057617221978553 * (278.132843 - pos_y) #posso alterar o ultimo numero do coeficiente pra dar mais precisao (tentativa erro)
+	# novo x: 39.170199, -7.873225
+	coord_x = -8.179169 - 0.000066936237282137 * (533.522217 - pos_x) #posso alterar o ultimo numero do coeficiente pra dar mais precisao (tentativa erro)
+	print(coord_y," ",coord_x)
